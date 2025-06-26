5c5404bcaeff1865370b74ad5709d338d76755d8
Arafat <arafat1234516@gmail.com>
2025-06-27 00:43:22 +0500

    Simple Scraping

--- webscraper/webscraper/spiders/condospider.py
+++ webscraper/webscraper/spiders/condospider.py
@@ -7,4 +7,13 @@ class CondospiderSpider(scrapy.Spider):
     start_urls = ["https://krisha.kz/prodazha/kvartiry/astana/"]
 
     def parse(self, response):
-        pass
+        cards = response.css("div.a-card")
+
+        for card in cards:
+            yield {
+            "price": card.css("div.a-card__price::text").get(),
+            "info": card.css("a.a-card__title::text").get(),
+            "address": card.css("div.a-card__subtitle::text").get(),
+            "description": card.css("div.a-card__text-preview::text").get()
+            "owner": card.css("div.a-card__owner").attrib["class"]
+            }
